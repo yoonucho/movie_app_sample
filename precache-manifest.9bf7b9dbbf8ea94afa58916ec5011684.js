4edf4149f109aa2c22a7530076c1027dd69de850
yoonucho <yoonu78@naver.com>
2018-12-19 22:18:55 +0900

    Updates

---
+++ precache-manifest.9bf7b9dbbf8ea94afa58916ec5011684.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "ce3138e24f56990b2357",
+    "url": "/movie_app/static/js/main.ce3138e2.chunk.js"
+  },
+  {
+    "revision": "bdbcfc72cb0207b3867a",
+    "url": "/movie_app/static/js/1.bdbcfc72.chunk.js"
+  },
+  {
+    "revision": "ce3138e24f56990b2357",
+    "url": "/movie_app/static/css/main.1d6208c1.chunk.css"
+  },
+  {
+    "revision": "ff4eb3d4fb6e43e27e313acfafbaf90d",
+    "url": "/movie_app/index.html"
+  }
+];
